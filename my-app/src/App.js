5f84af5280d24ef1fa152827f83353e3e890423f
Daniel <carlos_dan.gomez@uao.edu.co>
2023-10-10 09:59:39 -0500

    FirstApp base
    Importada de lugar previo para estructurar challenges en el repositorio

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import FirstApp from './FirstApp';
+
+function App() {
+  return (
+    <div className="App">
+      <React.StrictMode>
+        <FirstApp/>
+      </React.StrictMode>
+    </div>
+  );
+}
+
+
+
+export default App;
